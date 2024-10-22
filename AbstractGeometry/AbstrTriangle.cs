91517a5d5fa265c077116ab0521418cf1c1b6a95
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-10-22 21:30:36 +0300

    Triangles in progress

---
+++ AbstractGeometry/AbstrTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbstractGeometry
+{
+	abstract class AbstrTriangle:Shape
+	{
+		public abstract double GetHeight();
+		public AbstrTriangle(int startX, int startY, int lineWidth, Color color)
+			: base(startX, startY, lineWidth, color) { }
+		public override void Info(PaintEventArgs e)
+		{
+			Console.WriteLine(this.GetType());
+            Console.WriteLine($"Высота треугольника: {GetHeight()}");
+			base.Info(e);
+        }
+	}
+}
--- AbstractGeometry/Program.cs
+++ AbstractGeometry/Program.cs
@@ -33,6 +33,12 @@ namespace AbstractGeometry
 
 			Triangle triangle = new Triangle(70, 80, 100, 750, 300, 3, System.Drawing.Color.Blue);
 			triangle.Info(e);
+
+			IsoscalesTriangle i_triangle = new IsoscalesTriangle(50, 100, 600, 350, 3, System.Drawing.Color.Green);
+			i_triangle.Info(e);
+
+			EquilateralTriangle e_triangle = new EquilateralTriangle(150, 750, 350, 3, System.Drawing.Color.GreenYellow);
+			e_triangle.Info(e);
 		}
 		[DllImport("kernel32.dll")]
 		public static extern IntPtr GetConsoleWindow();
